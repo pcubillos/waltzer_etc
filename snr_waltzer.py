ce8af966c2ad1f5e38d14435dfa03e9045a6110e
pcubillos <patricio.cubillos@oeaw.ac.at>
2025-11-14 18:19:06 +0100

    Cleaned code. Added comments to output csv with telemetry/stats data. Produced pickle file with spectra for each target.

--- snr_waltzer.py
+++ snr_waltzer.py
@@ -1,20 +1,28 @@
 #! /usr/bin/env python
-# Copyright (c) 2025
+# Copyright (c) 2025 Sreejith and Patricio
 # LICENSE TBD
 # -*- coding: utf-8 -*-
 
-"""
-@author: saickara
-"""
+__all__ = [
+    'simulate_spectrum',
+    'find_closest_teff',
+    'photon_spectrum',
+    'flux_stats',
+    'snr_stats',
+    'main',
+]
 
 import sys
 import os
 import re
+import pickle
+import random
 
 import numpy as np
 import pandas as pd
 import pyratbay.constants as pc
 import pyratbay.spectrum as ps
+import scipy.interpolate as si
 
 
 def find_closest_teff(teff, base_dir='./models/'):
@@ -119,7 +127,11 @@ def photon_spectrum(
         bin_wl = 0.5 * (wl_max + wl_min)
         half_widths = 0.5 * (wl_max - wl_min)
         bin_flux = np.trapezoid(photons[mask], wl[mask])
-        return bin_wl, bin_flux, half_widths
+        return (
+            np.array([bin_wl]),
+            np.array([bin_flux]),
+            np.array([half_widths])
+        )
 
     # Spectroscopy
     i_min = np.searchsorted(bin_edges, wl_min)
@@ -189,22 +201,20 @@ def snr_stats(wl, flux, exp_time, n_obs):
     # Poisson noise estimation
     snr = total_flux / np.sqrt(total_flux)
 
-    # Photometry
-    if np.isscalar(total_flux) == 1:
-        trans_uncer = np.sqrt(2.0) / snr
-        return snr, snr, trans_uncer
-
     snr_mean = np.mean(snr)
     snr_max = np.max(snr)
-    trans_uncer = np.sqrt(2.0) / snr_mean
+    # Assume t_in=t_out and flux_in approx. flux_out:
+    trans_uncer = np.sqrt(2.0) / snr_mean / pc.ppm
 
-    return snr_mean, snr_max, trans_uncer
+    return snr_mean, snr_max, np.round(trans_uncer, 3)
 
 
 def main(
          csv_file=None,
          output_csv="waltzer_snr.csv",
          diameter=30.0,
+         efficiency=0.6,
+         t_dur=2.5,
          n_obs=10,
     ):
     """
@@ -247,8 +257,7 @@ def main(
         - VIS_transit_uncert   VIS transit uncertainty
         - NIR_mean_flux        Mean flux in NIR band (erg s-1 cm-2 A-1)
         - NIR_max_flux         Maximum flux in NIR band (erg s-1 cm-2 A-1)
-        - NIR_mean_snr         Mean SNR in NIR band
-        - NIR_max_snr          Maximum SNR in NIR band
+        - NIR_snr              SNR in NIR band
         - NIR_transit_uncert   NIR transit uncertainty
 
     csv_file = 'target_list_20250327.csv'
@@ -263,36 +272,44 @@ def main(
     Sec_obstr= 0.85  # Telescope secondary obstruction in % (1-Obstruction)
     R_d1     = 0.80  # Dichroic 1 Reflectance/Transmission in %
     R_d2     = 0.80  # Dichroic 2 Reflectance/Transmission in %
+
     R_uvfold = 1.00  # UV fold reflectance in %
     R_uvgr   = 0.90  # UV grating reflectance in %
     Uv_geff  = 0.65  # UV grating effeciency in %
     Uv_detQE = 0.55  # UV detector QE in %
+
     R_opfold = 0.90  # Optical fold reflectance in %
     R_opgr   = 0.90  # Optical grating reflectance in %
     Op_geff  = 0.75  # Optical grating effeciency in %
     Op_detQE = 0.90  # Optical detector QE in %
+
     BB_lens  = 0.90  # IR Broad band lens transmission in % IN REALITY, A FOLD
     BB_detQE = 0.85  # IR Broad band detector QE in %
 
-    # NUV effective area in cm^2
+    # Effective collecing areas in cm^2
     NUV_EFF_AREA = (
         primary_area * Rprim * Rsec * Sec_obstr *
         R_d1 * R_uvfold * R_uvgr * Uv_geff * Uv_detQE
     )
-    # Optical effective area in cm^2
     VIS_EFF_AREA = (
         primary_area * Rprim * Rsec * Sec_obstr *
         R_d1 * R_d2 * R_opfold * R_opgr * Op_geff * Op_detQE
     )
-    # NIR broadband effective area in cm^2
     NIR_EFF_AREA = (
         primary_area * Rprim * Rsec * Sec_obstr *
         R_d1 * R_d2 * BB_lens * BB_detQE
     )
 
-    # Exposure time in seconds (1 average transit duration with 60% efficiency)
-    efficiency = 0.6
-    exp_time = (60 * 60 * 2.5) * efficiency
+    # Wavelength ranges per band (angstrom)
+    nuv_wl_min = 2500.0
+    nuv_wl_max = 3300.0
+
+    vis_wl_min = 4160.0
+    vis_wl_max = 8200.0
+
+    nir_wl_min =  9000.0
+    nir_wl_max = 16000.0
+
 
     # Target list file path
     data = pd.read_csv(csv_file, delimiter=',', comment='#')
@@ -300,6 +317,8 @@ def main(
 
     # Extract required columns into individual arrays
     planet_names = data['pl_name']
+    #transit_depth = data['pl_trandep']
+    transit_dur = data['pl_trandur']
     stellar_temps = data['st_teff']
     stellar_radii = data['st_rad']
     stellar_masses = data['st_mass']
@@ -308,19 +327,30 @@ def main(
     distances = data['sy_dist']
     v_mags = data['sy_vmag']
 
+    # Total in-transit exposure time (s) [for .csv statistics]
+    if t_dur is None:
+        # Use CSV data for each target
+        exp_time = (transit_dur * 3600) * efficiency
+    else:
+        # Use fixed transit duration for all targets
+        exp_time = (t_dur * 3600) * efficiency
+        exp_time = np.tile(exp_time, ntargets)
+
     resolution = 60_000.0
     wl = ps.constant_resolution_spectrum(2_450, 20_000, resolution=resolution)
-    # WALTzER resolution and wavelength grid (angstrom)
+    # WALTzER resolution (FWHM) and wavelength grid (angstrom)
     inst_resolution = 3_000.0
     bin_edges = ps.constant_resolution_spectrum(
         2_500, 20_000, resolution=2.0*inst_resolution,
     )
 
     # Read models files
-    output_data = []
     cache_seds = {}
+    output_data = []
+    spectra = {}
     for i in range(ntargets):
-        print(f'Target {i+1}/{ntargets}: {repr(planet_names[i])}')
+        target = planet_names[i]
+        print(f'Target {i+1}/{ntargets}: {repr(target)}')
         star_R = stellar_radii[i] * pc.rsun
         star_dist = distances[i] * pc.parsec
         # Load SED model spectrum based on temperature
@@ -333,11 +363,10 @@ def main(
             spectrum = np.loadtxt(file, unpack=True)
             sed_wl, flux = spectrum[0:2]
             sed_flux = np.interp(wl, sed_wl, flux)
-            cache_seds[file] = sed_flux
-
-        conv_flux = ps.inst_convolution(
-            wl, sed_flux, inst_resolution, sampling_res=resolution,
-        )
+            conv_flux = ps.inst_convolution(
+                wl, sed_flux, inst_resolution, sampling_res=resolution,
+            )
+            cache_seds[file] = conv_flux
 
         # Convert flux from XX to erg s-1 cm-2 A-1
         # integrate over steradian
@@ -349,32 +378,34 @@ def main(
         )
 
         # Calculations for bands
-        nuv_wl_min = 2500.0
-        nuv_wl_max = 3300.0
         nuv_wl, nuv_flux, nuv_half_width = photon_spectrum(
             wl, flux, NUV_EFF_AREA, nuv_wl_min, nuv_wl_max, bin_edges,
         )
-        nuv_snr_stats = snr_stats(nuv_wl, nuv_flux, exp_time, n_obs)
-        nuv_flux_stats = flux_stats(wl, flux, nuv_wl_min, nuv_wl_max)
 
-        vis_wl_min = 4160.0
-        vis_wl_max = 8200.0
         vis_wl, vis_flux, vis_half_width = photon_spectrum(
             wl, flux, VIS_EFF_AREA, vis_wl_min, vis_wl_max, bin_edges,
         )
-        vis_snr_stats = snr_stats(vis_wl, vis_flux, exp_time, n_obs)
-        vis_flux_stats = flux_stats(wl, flux, vis_wl_min, vis_wl_max)
 
-        nir_wl_min =  8210.0
-        nir_wl_max = 16500.0
-        #nir_exp_time = 60 * 30.0
-        #nir_n_obs = 1
         nir_wl, nir_flux, nir_half_width = photon_spectrum(
             wl, flux, NIR_EFF_AREA, nir_wl_min, nir_wl_max, bin_edges,
             photometry=True,
         )
-        nir_snr_stats = snr_stats(nir_wl, nir_flux, exp_time, n_obs)[1:]
+
+        nuv_flux_stats = flux_stats(wl, flux, nuv_wl_min, nuv_wl_max)
+        vis_flux_stats = flux_stats(wl, flux, vis_wl_min, vis_wl_max)
         nir_flux_stats = flux_stats(wl, flux, nir_wl_min, nir_wl_max)
+
+        nuv_snr_stats = snr_stats(nuv_wl, nuv_flux, exp_time[i], n_obs)
+        vis_snr_stats = snr_stats(vis_wl, vis_flux, exp_time[i], n_obs)
+        nir_snr_stats = snr_stats(nir_wl, nir_flux, exp_time[i], n_obs)[1:]
+
+        spectra[target] = {
+            'wl': (nuv_wl, vis_wl, nir_wl),
+            'e_flux': (nuv_flux, vis_flux, nir_flux),
+            'wl_half_width': (nuv_half_width, vis_half_width, nir_half_width),
+            'transit_dur': transit_dur[i],
+            #'transit_depth': transit_depth[i],
+        }
 
         # Save results
         output_data.append([
@@ -386,6 +417,7 @@ def main(
             dec[i],
             distances[i],
             v_mags[i],
+            transit_dur[i],
             *nuv_flux_stats,
             *nuv_snr_stats,
             *vis_flux_stats,
@@ -396,7 +428,7 @@ def main(
 
     # Save result as CSV file
     header = (
-        "target, teff, r_star, m_star, ra, dec, distance, V_mag, "
+        "target, teff, r_star, m_star, ra, dec, distance, V_mag, transit_dur,"
         "NUV_mean_flux, NUV_max_flux,"
         "NUV_mean_snr, NUV_max_snr, NUV_transit_uncert, "
         "VIS_mean_flux, VIS_max_flux, "
@@ -405,16 +437,37 @@ def main(
     ).split(',')
     header = [h.strip() for h in header]
     units = (
-        "name, K, R_sun, M_sun, deg, deg, parsec, , "
-        "erg s-1 cm-2 A-1, erg s-1 cm-2 A-1, e-/s, e-/s, ,"
-        "erg s-1 cm-2 A-1, erg s-1 cm-2 A-1, e-/s, e-/s, ,"
-        "erg s-1 cm-2 A-1, erg s-1 cm-2 A-1, e-/s,"
+        "name, K, R_sun, M_sun, deg, deg, parsec, , h,"
+        "erg s-1 cm-2 A-1, erg s-1 cm-2 A-1, , , ppm,"
+        "erg s-1 cm-2 A-1, erg s-1 cm-2 A-1, , , ppm,"
+        "erg s-1 cm-2 A-1, erg s-1 cm-2 A-1, , ppm"
     ).split(',')
 
     with open(output_csv, 'w') as f:
+        # Telemetry and stats info
+        f.write(f"# primary diameter: {diameter} cm\n")
+        f.write(f"# duty-cycle efficiency: {efficiency}\n")
+        f.write(f"# instrumental resolution = lambda/FWHM = {inst_resolution}\n")
+
+        f.write("#\n# SNR stats are per HWHM! resolving element\n")
+        f.write(f"# Number of transits (for stats): {n_obs}\n")
+        if t_dur is None:
+            f.write(f"# total in-transit time (for stats): see 'transit_dur' column\n")
+        else:
+            f.write(f"# total in-transit time (for stats): {t_dur} h\n")
+
+        f.write("#\n# Wavelength ranges per band (angstrom)\n")
+        f.write(f"# NUV  {nuv_wl_min} {nuv_wl_max}\n")
+        f.write(f"# VIS  {vis_wl_min} {vis_wl_max}\n")
+        f.write(f"# NIR  {nir_wl_min} {nir_wl_max}\n#\n")
         f.write(','.join(header) + '\n')
         f.write(', '.join(units) + '\n')
         np.savetxt(f, output_data, delimiter=",", fmt="%s")
+
+    p_file = output_csv.replace('.csv', '.pickle')
+    with open(p_file, 'wb') as handle:
+        pickle.dump(spectra, handle, protocol=4)
+
 
 
 if __name__ == '__main__':
