0e7a60feb88d02dc5dfdfd8339907b6708c02081
pcubillos <patricio.cubillos@oeaw.ac.at>
2026-01-23 17:24:26 +0100

    updated copyright text

--- waltzer_etc/gui_plotly.py
+++ waltzer_etc/gui_plotly.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Patricio Cubillos and A. G. Sreejith
+# Copyright (c) 2025-2026 Patricio Cubillos and A. G. Sreejith
 # WALTzER is open-source software under the GPL-2.0 license (see LICENSE)
 
 from itertools import groupby
